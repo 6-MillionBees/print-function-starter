e1fb902c88c7086ec138f473fee6da35dc139517
6-MillionBees <BoettcherAr321@northwested.org>
2024-09-09 09:33:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+# Arden boettcher
+# 9/9/2024
+# Printing in Python
 
+# excersise 2 - 3 
+
+# excersise 2 - 5 "The only thing we have to fear is fear itself! And giant spiders" - Quinton Trembly
+
+# excersise 2 - 6 "Burger" - Hideo Kojima
