110c041ac818e26516884dc45f0819a3874ec5c3
6-MillionBees <BoettcherAr321@northwested.org>
2024-09-09 10:14:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,12 @@
 # Printing in Python
 
 # excersise 2 - 3 
+name = 'Eric'
+print(f'how are you doing today {name}')
 
 # excersise 2 - 5 "The only thing we have to fear is fear itself! And giant spiders" - Quinton Trembly
 
+
+
 # excersise 2 - 6 "Burger" - Hideo Kojima
+
